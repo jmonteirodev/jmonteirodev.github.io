d1f774a37e8a0c7b391cb6813aeb801300d7e13e
Francisco Jonathan Vasconcelos Monteiro <jonathanmonteirodev@gmail.com>
2020-03-13 09:47:25 -0300

    Add files via upload

--- js/infos.js
+++ js/infos.js
@@ -3,13 +3,15 @@ var atividades = [
         titulo_curto : 'Atividade 1',
         titulo : 'Atividade 1: Ilusão',
         texto : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora praesentium consequuntur doloremque numquam magnam impedit dolorem expedita, doloribus odio repudiandae, corporis nemo commodi modi provident quasi. Aut cum error molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum unde ipsum delectus beatae dicta eligendi maxime recusandae fugit quod provident debitis asperiores, corporis quasi aliquid. Cum nostrum at sequi libero!',
-        imagem : 'img/blog-img/b18.jpg'
+        imagem : 'img/blog-img/b18.jpg',
+        texto_curto : 'testeeee'
     },
     {   
         titulo_curto : 'Atividade 2',
         titulo : 'Atividade 2: Arroz',
         texto : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora praesentium consequuntur doloremque numquam magnam impedit dolorem expedita, doloribus odio repudiandae, corporis nemo commodi modi provident quasi. Aut cum error molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum unde ipsum delectus beatae dicta eligendi maxime recusandae fugit quod provident debitis asperiores, corporis quasi aliquid. Cum nostrum at sequi libero!',
-        imagem : 'img/blog-img/b18.jpg'
+        imagem : 'img/blog-img/b18.jpg',
+        texto_curto : ''
     }
 ];
 var trabalhos = [];
