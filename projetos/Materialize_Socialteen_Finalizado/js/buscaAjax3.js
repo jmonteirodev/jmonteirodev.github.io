7bbb63f1beab80ee84d65963906b1ff2b6b4efa1
Jonathan Monteiro <jonathanmonteirodev@gmail.com>
2020-05-20 18:10:38 -0300

    Social teen

--- projetos/Materialize_Socialteen_Finalizado/js/buscaAjax3.js
+++
@@ -1,5 +0,0 @@
-var coisa = document.getElementsByClassName("coisa");
-window.onload = function(){
-	requisitarArquivo("../../busca3.html",coisa[0]);
-	requisitarArquivo("../../busca3.html",coisa[1]);
-};
